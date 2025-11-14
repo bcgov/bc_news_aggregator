5c9fc24f76567a7dc4039afeab4bac23857799d7
Rich-P-Martin <richard.martin@gov.bc.ca>
2025-11-14 14:21:12 -0800

    move to shinyapps, as Rpubs cannot update automatically from linux.

--- 01_preprocess.R
+++ 01_preprocess.R
@@ -7,8 +7,8 @@ library(here)
 #constants-----------------------
 #Free teer of https://newsapi.org/ only allows 100 queries... get a bunch of keys for development
 #api_key <- "5b6bbdb71cee445482b86c36349a0776" #rich.p.martin
-#api_key <- "8163843a603d4703b5e8c6a38a6040ee" #richard.martin@gov.bc.ca
-api_key <- "c7e22e0d769342c2a7503d2f90480795" #psfs.forecast.coop@gmail.com
+api_key <- "8163843a603d4703b5e8c6a38a6040ee" #richard.martin@gov.bc.ca
+#api_key <- "c7e22e0d769342c2a7503d2f90480795" #psfs.forecast.coop@gmail.com
 #api_key <- "63ce890f43384082b0fed1c8b3256d78" #sophia
 
 blacklist_sources=c("Biztoc.com")
@@ -144,17 +144,10 @@ new_scrape <- anti_join(possibly_already_scraped, previously_scraped, by="url")|
  if(nrow(new_scrape)>0){
    bind_rows(previously_scraped, new_scrape)|>
      write_rds("scraped.rds")
-   rmarkdown::render("bc_news_dashboard.Rmd", output_format = "html_document")
-   result <- markdown::rpubsUpload(
-     title = "News Aggregator",
-     htmlFile = "bc_news_dashboard.html",
-     id = "1366125"
+   rsconnect::deployApp(
+     appDir = "bc_news_dashboard.Rmd",
+     appName = "bc_news_dashboard",
+     account = "richard-martin"
    )
-  }
-
-# previously_scraped|> #to reset previously scraped
-#   filter(source=="rich")|>
-#   write_rds("scraped.rds")
-#
-
+}
 
